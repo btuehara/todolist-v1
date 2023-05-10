789939817083ffd9d3d88f431bcc42305dddef85
btuehara <122405572+btuehara@users.noreply.github.com>
2023-05-09 14:55:13 -1000

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,25 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+//img
 
 // create a date object that requires the date.js file
 const date = require(__dirname + "/date.js");
-
+const final = require(__dirname + "/finals.js");
 const app = express();
 
 // set an array for the default items in the list
-let items = ["Buy Food", "Prepare Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Prepare Food", "Cook Food", "Eat Food", "Clean Plates"];
 // set an empty array for new work items
-let workItems = ["Show Up"];
-// create new array for fun items
-let funItems = ["Watch TV", "Read a Book"];
-// set an empty array for new weekend items
-let weekendItems = ["Relax", "Watch TV"];
+let workItems = ["Show Up", "Get Settled", "Drink Coffee"];
+
+// setup an array for Fun 
+let funItems = ["game", "trade", "golf"];
+
+// setup an array for Weekend
+let weekendItems = ["fishing", "cook", "movies"];
+
+let finalExamItems = ["ICS 385", "ICS 360", "BUS 320"];
 
 // set EJS as the viewing engine to display html
 app.set('view engine', 'ejs');
@@ -44,47 +49,62 @@ app.post("/", function(req, res) {
     let item = req.body.newItem;
     
     // if route is /work, add to work list
+  // if list === Fun then go to /fun
+  // if list ==== Weekend then go to /weekend
+
+  // === "directory you are using"
     if (req.body.list === "Work") {
         workItems.push(item);
         res.redirect("/work");
     } 
-    
-    // if route is /fun, add to fun list
+    //res.redirect("/work") looks for the word "Work" in the ListTitle
+    //workItems.push(item) sends new list todo item to the "workItems" list
+      
+       // if not = work, then fun
     else if (req.body.list === "Fun") {
         funItems.push(item);
         res.redirect("/fun");
-    } 
-
-    // if route is /weekend, add to fun list
+    }
+      // if not = fun, then weekend
     else if (req.body.list === "Weekend") {
         weekendItems.push(item);
         res.redirect("/weekend");
     }
-
+      else if (req.body.list === "Finals") {
+        finalExamItems.push(item);
+        res.redirect("/finals");
+    }
+  
     else {
         items.push(item);
         res.redirect("/");
     }
 });
 
-// display work to do list on the localhost:3000/work route!
+// display default to do list on the localhost:3000/work route!
 app.get("/work", function(req, res){
 
   let day = date.getDate();
-    res.render("list", {listTitle: "Work To Do List", newListItems: workItems})
+  
+    res.render("list", {listTitle: "Work Items To-Do List", newListItems: workItems})
 });
 
-// display fun to do list
+// add a app.get for  route fun
 app.get("/fun", function(req, res){
 
-  let day = date.getDate();
-    res.render("list", {listTitle: "Fun To Do List", newListItems: funItems})
+  let date = date.getDate();
+  
+    res.render("list", {listTitle: "Fun To-Do List", newListItems: funItems})
 });
 
-// display weekend to do list
+// add a app.get for route weekend
 app.get("/weekend", function(req, res){
-    res.render("list", {listTitle: "Weekend To Do List", newListItems: weekendItems})
+
+  let day = date.getDate();
+  
+    res.render("list", {listTitle: "Weekend To-Do List", newListItems: weekendItems})
 });
+// Make sure your listTitle starts off with Fun Items and Weekend Items
 
 app.listen(3000, function() {
 console.log ("Server is running on port 3000")
